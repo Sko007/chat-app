f83e0c15ec54e76141eaa45608ff1d5ad55b48f1
Severin <severin_koch@gmx.de>
2019-12-13 11:14:29 +0100

    making asny await

--- message/router.js
+++ message/router.js
@@ -2,29 +2,37 @@ const express = require("express");
 
 const Message = require("./model");
 
-const { Router } = express
+const { Router } = express;
 
 const router = new Router();
 
-router.get("/message", (req, res, next) => {
-  Message.findAll()
-    .then(message => {
-      res.send(message);
-    })
-
+router.get("/message", async (req, res, next) => {
+    
+    const messages = await Message.findAll()
+            
+    res.send(messages)
+ 
     .catch(next);
 });
 
+// normal fetch without async await
 
-router.post("/message", (req,res, next) => {
+// router.get("/message", (req, res, next) => {
+//     Message.findAll()
+//       .then(message => {
+//         res.send(message);
+//       })
+  
+//       .catch(next);
+//   });
 
-    Message.create(req.body)
-    .then(message=> {
-        res.send("message")
-    })
-    .catch(next)
 
 
-})
+router.post("/message", async (req, res, next) => {
+  const message = 
+  await Message.create(req.body)
+    res.send(message)
+  .catch(next);
+});
 
 module.exports = router;
